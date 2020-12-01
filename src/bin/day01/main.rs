2c72d40ffb79db33258b4a0daf0c899725c38392
chryslovelace <chryslovelace@gmail.com>
2020-12-01 12:56:27 -0500

    day 1

---
+++ src/bin/day01/main.rs
@@ -0,0 +1,32 @@
+use itertools::Itertools;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref INPUT: Vec<u32> = include_str!("input.txt")
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect();
+}
+
+fn part1() {
+    let (a, b) = INPUT
+        .iter()
+        .tuple_combinations()
+        .find(|(&a, &b)| a + b == 2020)
+        .unwrap();
+    println!("*   {}", a * b);
+}
+
+fn part2() {
+    let (a, b, c) = INPUT
+        .iter()
+        .tuple_combinations()
+        .find(|(&a, &b, &c)| a + b + c == 2020)
+        .unwrap();
+    println!("**  {}", a * b * c);
+}
+
+fn main() {
+    part1();
+    part2();
+}
