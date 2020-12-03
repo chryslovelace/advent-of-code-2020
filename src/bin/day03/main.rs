5f0d0fe39e0554c1410180e111715069956657e7
chryslovelace <chryslovelace@gmail.com>
2020-12-03 12:23:25 -0500

    day 3

---
+++ src/bin/day03/main.rs
@@ -0,0 +1,69 @@
+use lazy_static::lazy_static;
+use std::collections::BTreeSet;
+
+#[derive(Debug)]
+struct TreeMap {
+    trees: BTreeSet<(usize, usize)>,
+    height: usize,
+    width: usize,
+}
+
+impl TreeMap {
+    fn tree_at(&self, (x, y): (usize, usize)) -> bool {
+        self.trees.contains(&(x % self.width, y))
+    }
+
+    fn trees_encountered(&self, slope: (usize, usize)) -> usize {
+        let mut pos = (0, 0);
+        let mut trees_encountered = 0;
+        while pos.1 < self.height {
+            if self.tree_at(pos) {
+                trees_encountered += 1;
+            }
+            pos.0 += slope.0;
+            pos.1 += slope.1;
+        }
+        trees_encountered
+    }
+}
+
+lazy_static! {
+    static ref TREE_MAP: TreeMap = {
+        let input = include_str!("input.txt");
+        let mut height = 0;
+        let mut width = 0;
+        let mut trees = BTreeSet::new();
+        for (y, line) in input.lines().enumerate() {
+            height += 1;
+            width = line.len();
+            for (x, ch) in line.chars().enumerate() {
+                if ch == '#' {
+                    trees.insert((x, y));
+                }
+            }
+        }
+        TreeMap {
+            trees,
+            height,
+            width,
+        }
+    };
+}
+
+fn part1() {
+    println!("*   {}", TREE_MAP.trees_encountered((3, 1)));
+}
+
+fn part2() {
+    let tree_product = TREE_MAP.trees_encountered((1, 1))
+        * TREE_MAP.trees_encountered((3, 1))
+        * TREE_MAP.trees_encountered((5, 1))
+        * TREE_MAP.trees_encountered((7, 1))
+        * TREE_MAP.trees_encountered((1, 2));
+    println!("**  {}", tree_product);
+}
+
+fn main() {
+    part1();
+    part2();
+}
