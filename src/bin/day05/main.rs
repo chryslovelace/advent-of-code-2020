1e2096c145ad31c7899f521ab880ed168581651d
Chrys Lovelace <chryslovelace@gmail.com>
2020-12-05 16:27:12 -0500

    day 5

---
+++ src/bin/day05/main.rs
@@ -0,0 +1,37 @@
+use itertools::Itertools;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref SEAT_IDS: Vec<usize> = include_str!("input.txt")
+        .lines()
+        .map(|line| usize::from_str_radix(
+            &line
+                .replace('B', "1")
+                .replace('F', "0")
+                .replace('R', "1")
+                .replace('L', "0"),
+            2,
+        )
+        .unwrap())
+        .collect();
+}
+
+fn part1() {
+    let max_seat_id = SEAT_IDS.iter().max().unwrap();
+    println!("*   {}", max_seat_id);
+}
+
+fn part2() {
+    let (seat_before, _) = SEAT_IDS
+        .iter()
+        .sorted()
+        .tuple_windows()
+        .find(|(&a, &b)| a + 2 == b)
+        .unwrap();
+    println!("**  {}", seat_before + 1);
+}
+
+fn main() {
+    part1();
+    part2();
+}
