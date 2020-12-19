4e241165fba9170a95b27c2dc6df0100aabae304
Chrys Lovelace <chryslovelace@gmail.com>
2020-12-19 10:26:44 -0500

    no need for ast

--- src/bin/day18/main.rs
+++ src/bin/day18/main.rs
@@ -1,44 +1,28 @@
 use lazy_static::lazy_static;
 
-pub enum Expr {
-    Literal(i64),
-    Sum(Box<Expr>, Box<Expr>),
-    Product(Box<Expr>, Box<Expr>),
-}
-
 peg::parser! {
     grammar arithmetic() for str {
         rule _() = [' ' | '\n']*
 
-        pub rule expression1() -> Expr = precedence! {
-            l:(@) "*" r:@ { Expr::Product(Box::new(l), Box::new(r)) }
-            l:(@) "+" r:@ { Expr::Sum(Box::new(l), Box::new(r)) }
+        pub rule expression1() -> i64 = precedence! {
+            l:(@) "*" r:@ { l * r }
+            l:(@) "+" r:@ { l + r }
             --
             _ n:literal() _ { n }
             _ "(" _ e:expression1() _ ")" _ { e }
         }
 
-        pub rule expression2() -> Expr = precedence! {
-            l:(@) "*" r:@ { Expr::Product(Box::new(l), Box::new(r)) }
+        pub rule expression2() -> i64 = precedence! {
+            l:(@) "*" r:@ { l * r }
             --
-            l:(@) "+" r:@ { Expr::Sum(Box::new(l), Box::new(r)) }
+            l:(@) "+" r:@ { l + r }
             --
             _ n:literal() _ { n }
             _ "(" _ e:expression2() _ ")" _ { e }
         }
 
-        rule literal() -> Expr
-            = n:$(['0'..='9']+) { Expr::Literal(n.parse().unwrap()) }
-    }
-}
-
-impl Expr {
-    fn eval(&self) -> i64 {
-        match self {
-            Expr::Literal(n) => *n,
-            Expr::Sum(l, r) => l.eval() + r.eval(),
-            Expr::Product(l, r) => l.eval() * r.eval(),
-        }
+        rule literal() -> i64
+            = n:$(['0'..='9']+) { n.parse().unwrap() }
     }
 }
 
@@ -51,7 +35,7 @@ fn part1() {
         "*   {}",
         HOMEWORK
             .iter()
-            .map(|expr| arithmetic::expression1(expr).unwrap().eval())
+            .map(|expr| arithmetic::expression1(expr).unwrap())
             .sum::<i64>()
     );
 }
@@ -61,7 +45,7 @@ fn part2() {
         "**  {}",
         HOMEWORK
             .iter()
-            .map(|expr| arithmetic::expression2(expr).unwrap().eval())
+            .map(|expr| arithmetic::expression2(expr).unwrap())
             .sum::<i64>()
     );
 }
