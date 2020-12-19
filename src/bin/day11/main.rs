5cb5108ae40875fe34395d14073b672c74a26496
Chrys Lovelace <chryslovelace@gmail.com>
2020-12-19 15:28:51 -0500

    day 11 part 2

--- src/bin/day11/main.rs
+++ src/bin/day11/main.rs
@@ -9,30 +9,37 @@ enum Seat {
     Occupied,
 }
 
-#[derive(Clone, Hash)]
+#[derive(Clone)]
 struct Seats {
     seats: BTreeMap<(i32, i32), Seat>,
     width: usize,
     height: usize,
 }
 
+const DIRECTIONS: [(i32, i32); 8] = [
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+    (-1, 0),
+    (1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+];
+
 impl Seats {
     fn is_occupied(&self, x: i32, y: i32) -> bool {
         matches!(self.seats.get(&(x, y)), Some(Seat::Occupied))
     }
 
-    fn occupied_adjacent(&self, x: i32, y: i32) -> u8 {
-        self.is_occupied(x - 1, y - 1) as u8
-            + self.is_occupied(x, y - 1) as u8
-            + self.is_occupied(x + 1, y - 1) as u8
-            + self.is_occupied(x + 1, y) as u8
-            + self.is_occupied(x + 1, y + 1) as u8
-            + self.is_occupied(x, y + 1) as u8
-            + self.is_occupied(x - 1, y + 1) as u8
-            + self.is_occupied(x - 1, y) as u8
+    fn occupied_adjacent(&self, x: i32, y: i32) -> usize {
+        DIRECTIONS
+            .iter()
+            .filter(|(dx, dy)| self.is_occupied(x + dx, y + dy))
+            .count()
     }
 
-    fn next_seats(&self) -> Self {
+    fn next_seats_v1(&self) -> Self {
         let seats = self
             .seats
             .iter()
@@ -54,19 +61,55 @@ impl Seats {
         }
     }
 
+    fn occupied_visible(&self, x: i32, y: i32) -> usize {
+        DIRECTIONS
+            .iter()
+            .filter(|(dx, dy)| {
+                matches!(
+                    std::iter::successors(Some((x + dx, y + dy)), |(x, y)| Some((x + dx, y + dy)))
+                        .take_while(|(x, y)| {
+                            (0..self.width as i32).contains(x)
+                                && (0..self.height as i32).contains(y)
+                        })
+                        .find_map(|pos| self.seats.get(&pos)),
+                    Some(Seat::Occupied)
+                )
+            })
+            .count()
+    }
+
+    fn next_seats_v2(&self) -> Self {
+        let seats = self
+            .seats
+            .iter()
+            .map(|(&(x, y), &seat)| {
+                (
+                    (x, y),
+                    match seat {
+                        Seat::Empty if self.occupied_visible(x, y) == 0 => Seat::Occupied,
+                        Seat::Occupied if self.occupied_visible(x, y) >= 5 => Seat::Empty,
+                        _ => seat,
+                    },
+                )
+            })
+            .collect();
+        Self {
+            seats,
+            width: self.width,
+            height: self.height,
+        }
+    }
+
     fn count_occupied(&self) -> usize {
         self.seats
             .values()
-            .filter(|seat| match seat {
-                Seat::Empty => false,
-                Seat::Occupied => true,
-            })
+            .filter(|seat| matches!(seat, Seat::Occupied))
             .count()
     }
 
     fn hash(&self) -> u64 {
         let mut hasher = DefaultHasher::new();
-        <Self as Hash>::hash(self, &mut hasher);
+        self.seats.hash(&mut hasher);
         hasher.finish()
     }
 }
@@ -114,7 +157,7 @@ fn part1() {
     let mut seats = INITIAL_SEATS.clone();
     let mut last_seats_hash = seats.hash();
     loop {
-        seats = seats.next_seats();
+        seats = seats.next_seats_v1();
         let seats_hash = seats.hash();
         if seats_hash == last_seats_hash {
             break;
@@ -124,6 +167,21 @@ fn part1() {
     println!("*   {}", seats.count_occupied());
 }
 
+fn part2() {
+    let mut seats = INITIAL_SEATS.clone();
+    let mut last_seats_hash = seats.hash();
+    loop {
+        seats = seats.next_seats_v2();
+        let seats_hash = seats.hash();
+        if seats_hash == last_seats_hash {
+            break;
+        }
+        last_seats_hash = seats_hash;
+    }
+    println!("**  {}", seats.count_occupied());
+}
+
 fn main() {
     part1();
+    part2();
 }
